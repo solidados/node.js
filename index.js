321dd9a4327f9912aefcb9d2b0d6da28de7e5724
solidados <geekbrains.pk@internet.ru>
2023-12-29 21:07:33 +0400

    feat: create framework and export class Router

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
 const http = require('http')
 const EventEmitter = require('events')
+const Router = require('./framework/Router')
 
 const PORT = process.env.PORT || 5000
+
 const emitter = new EventEmitter()
-
-class Router {
-  constructor () {
-    this.endpoints = {}
-  }
-
-  request (method = "GET", path, handler) {
-    if (!this.endpoints[path]) {
-      this.endpoints[path] = {}
-    }
-    /** /users [GET, POST, DELETE]; /posts [GET, POST, PUT, DELETE] */
-    const endpoint = this.endpoints[path]
-
-    if (endpoint[method]) {
-      throw new Error(`[${method}] по адресу ${path} уже существует`)
-    }
-
-    endpoint[method] = handler
-    emitter.on(`[${path}]:[${method}]`, (req, res) => {
-      handler(req, res)
-    })
-  }
-
-  get (path, handler) {
-    this.request('GET', path, handler)
-  }
-
-  post (path, handler) {
-    this.request('POST', path, handler)
-  }
-
-  put (path, handler) {
-    this.request('PUT', path, handler)
-  }
-
-  delete (path, handler) {
-    this.request('DELETE', path, handler)
-  }
-}
-
 const router = new Router()
 
 router.get('/users', (req, res) => {
