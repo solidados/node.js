a79df7b8848c1cfe75548c0a74e4b5476369a13f
solidados <geekbrains.pk@internet.ru>
2023-12-29 20:08:13 +0400

    feat: add server-side rendering

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const http = require('http')
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
-
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8'
+  })
+  res.end('<h2>Server is running...</h2>')
 })
 
 server.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
