565eb1ab98f32d0091a79a9d5bd644a27f568b01
solidados <geekbrains.pk@internet.ru>
2023-12-29 20:20:34 +0400

    feat: display JSON content on page

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ const http = require('http')
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/users') return res.end('USERS')
-  if (req.url === '/posts') return res.end('POSTS')
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  })
+  if (req.url === '/users') {
+    return res.end(JSON.stringify([
+      {
+        name: 'John',
+        surname: 'Smith',
+        age: 25,
+        id: 1
+      }
+    ]))
+  }
+  if (req.url === '/posts') {
+    return res.end(JSON.stringify([
+      {
+        postId: 1,
+        authorId: 1,
+        title: 'Lorem ipsum dolor sit amet.',
+        post: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet deserunt enim eveniet ex exercitationem hic, nam nulla quas repellendus tempora voluptates? Delectus esse maiores mollitia, nam officia possimus provident tempora? Autem commodi dicta facere in neque nihil obcaecati quam recusandae repellat voluptatum? Architecto delectus, explicabo minus officiis reiciendis repellendus.'
+      }
+    ]))
+  }
   res.end(req.url)
 })
 
