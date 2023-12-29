94cbaf0b28be1a7eaa452ce257527a7417299a3b
solidados <geekbrains.pk@internet.ru>
2023-12-29 20:11:12 +0400

    feat: simple generate end ponts

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const http = require('http')
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
-  })
-  res.end('<h2>Server is running...</h2>')
+  if (req.url === '/users') return res.end('USERS')
+  if (req.url === '/posts') return res.end('POSTS')
+  res.end(req.url)
 })
 
 server.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
