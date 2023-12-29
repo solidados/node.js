3663f2ac37bb89c58058afdfcd25d8d64d14b594
solidados <geekbrains.pk@internet.ru>
2023-12-29 20:29:57 +0400

    feat: create class Router

--- index.js
+++ index.js
@@ -2,30 +2,28 @@ const http = require('http')
 
 const PORT = process.env.PORT || 5000
 
+class Router {
+  constructor () {
+    this.endpoints = {}
+  }
+
+  request (method = "GET", path, handler) {
+    if (!this.endpoints[path]) {
+      this.endpoints[path] = {}
+    }
+    /** /users [GET, POST, DELETE]; /posts [GET, POST, PUT, DELETE] */
+    const endpoint = this.endpoints[path]
+
+    if (endpoint[method]) {
+      throw new Error(`[${method}] по адресу ${path} уже существует`)
+    }
+
+    endpoint[method] = handler
+  }
+}
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-  })
-  if (req.url === '/users') {
-    return res.end(JSON.stringify([
-      {
-        name: 'John',
-        surname: 'Smith',
-        age: 25,
-        id: 1
-      }
-    ]))
-  }
-  if (req.url === '/posts') {
-    return res.end(JSON.stringify([
-      {
-        postId: 1,
-        authorId: 1,
-        title: 'Lorem ipsum dolor sit amet.',
-        post: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet deserunt enim eveniet ex exercitationem hic, nam nulla quas repellendus tempora voluptates? Delectus esse maiores mollitia, nam officia possimus provident tempora? Autem commodi dicta facere in neque nihil obcaecati quam recusandae repellat voluptatum? Architecto delectus, explicabo minus officiis reiciendis repellendus.'
-      }
-    ]))
-  }
+
   res.end(req.url)
 })
 
