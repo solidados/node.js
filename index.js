a75b392b8c21dd5d563d31a75a84e3bb312a7b17
solidados <geekbrains.pk@internet.ru>
2023-12-30 13:40:56 +0400

    feat: add userRouter to index.js

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const http = require('http')
 const Router = require('./framework/Router')
 const Application = require('./framework/Application')
+const userRouter = require('./src/user-router')
 
 const PORT = process.env.PORT || 5000
 
-const router = new Router()
 const app = new Application()
 
-router.get('/users', (req, res) => {
-  res.end('YOU HAVE SENT A REQUEST TO /users')
-})
-
-router.get('/posts', (req, res) => {
-  res.end('YOU HAVE SENT A REQUEST TO /posts')
-})
-
-app.addRouter(router)
+app.addRouter(userRouter)
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
