ab6b067a1c956f4bfc34c289253fcdb2e071436f
solidados <geekbrains.pk@internet.ru>
2023-12-30 13:46:17 +0400

    feat: import jsonParser to use in index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const Router = require('./framework/Router')
 const Application = require('./framework/Application')
+const jsonParser = require('./framework/parseJson')
 const userRouter = require('./src/user-router')
 
 const PORT = process.env.PORT || 5000
 
 const app = new Application()
 
+app.use(jsonParser)
 app.addRouter(userRouter)
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
